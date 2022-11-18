bbd81b0197bf629c510bad0ac33bc115baa0843d
Mitch Oz <divine_comedian@protonmail.com>
2022-11-18 14:21:31 -0600

    add token abis

---
+++ groupInfo.ts
@@ -0,0 +1,7 @@
+const masterMitch = "0x62Bb362d63f14449398B79EBC46574F859A6045D"
+const voterMitch = "0x06263e1A856B36e073ba7a50D240123411501611"
+const redrumAddress = "0xF185087bE41f7Ae83690998f97C8c512b3a55f00"
+const haliAddress = "0x88476d163Bc73f465eFd937EE60855e1BB1c3Bb7"
+
+
+export {haliAddress, masterMitch, voterMitch, redrumAddress}
